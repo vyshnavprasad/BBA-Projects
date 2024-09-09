6b23d2f8bf1567a3dd28305e6befc39b9639f2a2
Vyshnav Prasad <vyshnavprasad.va@gmail.com>
2024-09-09 18:54:43 +0530

    Update

--- 10_CurrencyConverter/script.js
+++ 10_CurrencyConverter/script.js
@@ -21,11 +21,31 @@ const countryOne = document.querySelector("#countryOne");
 const countryOneAmount = document.querySelector("#countryOneAmount");
 const countryTwoAmount = document.querySelector("#countryTwoAmount");
 const countryTwo = document.querySelector("#countryTwo");
-const refresh = document.querySelector("#generate");
-let currencyOne = "INR", currencyTwo = "USD";
+const calculate = document.querySelector("#calculate");
+let currencyOne, currencyTwo;
 
 
 //FUNCTIONS
+
+
+
+// EVENT FUNCTIONS
+function Calculate(){
+    currencyOne = countryOne.value;
+    currencyTwo = countryTwo.value;
+    let result;
+    fetch(`https://v6.exchangerate-api.com/v6/2a52a7fb4ba56d5c83393733/pair/${currencyOne}/${currencyTwo}`)
+    .then(response => response.json())
+    .then(data => {
+        result = data.conversion_rate*countryOneAmount.value;
+        countryTwoAmount.innerText = result;
+    })
+    .catch(error => console.error("Error Fetch!!!", error))
+}
+
+createCountryList(countryOne);
+createCountryList(countryTwo);
+
 function createCountryList(element){
     fetch("https://restcountries.com/v3.1/all?fields=name,currencies,flag")
     .then(response => response.json())
@@ -43,26 +63,21 @@ function createCountryList(element){
 }
 
 
-
-// EVENT FUNCTIONS
-function EventRun(){
-    
-}
-
-createCountryList(countryOne);
-createCountryList(countryTwo);
-
-
-
 // EVENT HANDLERS
 
 calculate.addEventListener("click", () => {
     let result;
+    currencyOne = countryOne.value;
+    currencyTwo = countryTwo.value;
     fetch(`https://v6.exchangerate-api.com/v6/2a52a7fb4ba56d5c83393733/pair/${currencyOne}/${currencyTwo}`)
     .then(response => response.json())
     .then(data => {
         result = data.conversion_rate*countryOneAmount.value;
         countryTwoAmount.innerText = result;
+        console.log(result)
     })
     .catch(error => console.error("Error Fetch!!!", error))
 })
+
+countryTwo.addEventListener("change", () =>Calculate());
+countryOne.addEventListener("change", () =>Calculate());
